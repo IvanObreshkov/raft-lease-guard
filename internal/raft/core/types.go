9397ce2c77cb75c27e4882710fa4ca527b18ee0a
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2026-07-24 10:59:43 +0300

    Make ServerID a unit64

--- internal/raft/core/types.go
+++ internal/raft/core/types.go
@@ -1,7 +1,7 @@
 package core
 
 // ServerID is the ID of a server in the cluster
-type ServerID int64
+type ServerID uint64
 
 // ServerState is the state of a server at any given point: Follower, Candidate, or Leader, as per Section 5.1 from the
 // [Raft paper](https://raft.github.io/raft.pdf).
