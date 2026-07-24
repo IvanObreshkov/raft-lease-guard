7bf60c26e3ad6f1b18dc59142a837c6e3b82c5ee
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2026-07-24 11:07:02 +0300

    Create RaftServer struct and add TODOs

--- internal/raft/core/raft.go
+++ internal/raft/core/raft.go
@@ -1,6 +1,10 @@
 package core
 
-import raftpb "github.com/IvanObreshkov/raft-lease-guard/internal/raft/proto"
+import (
+	"time"
+
+	raftpb "github.com/IvanObreshkov/raft-lease-guard/internal/raft/proto"
+)
 
 // RaftPersistedState is the state of a Raft server persisted on stable storage before responding to RPCs
 // See: Figure 2 from the [Raft paper](https://raft.github.io/raft.pdf)
@@ -14,8 +18,8 @@ type RaftPersistedState struct {
 	// voted, as per Figure 2 from the [Raft paper](https://raft.github.io/raft.pdf). Votes are per term, so it is reset
 	// to nil whenever CurrentTerm changes.
 	VotedFor *ServerID
-	// Log holds the log entries; each entry contains a command for the state machine and the term when the entry was
-	// received by the leader. The first index is 1, as per Figure 2 and Section 5.3 from the
+	// Log holds the log entries; each entry contains a command for the replicated state machine and the term when the
+	// entry was received by the leader. The first index is 1, as per Figure 2 and Section 5.3 from the
 	// [Raft paper](https://raft.github.io/raft.pdf).
 	Log []*raftpb.LogEntry
 }
@@ -40,10 +44,10 @@ type RaftVolatileStateLeader struct {
 	MatchIndex map[ServerID]uint64
 }
 
-// Raft is the complete state of a Raft server: the persistent and volatile state that every server keeps, the volatile
+// RaftState is the complete state of a Raft server: the persistent and volatile state that every server keeps, the volatile
 // state kept only while it is the leader, and its current ServerState. See Figure 2 from the
 // [Raft paper](https://raft.github.io/raft.pdf).
-type Raft struct {
+type RaftState struct {
 	RaftPersistedState
 	RaftVolatileState
 	// LeaderState is the volatile leader state; it is non-nil only while State is Leader, and is reinitialized after
@@ -52,4 +56,16 @@ type Raft struct {
 	// State is the current state of the server: Follower, Candidate, or Leader, as per Section 5.1 from the
 	// [Raft paper](https://raft.github.io/raft.pdf). A server starts as a Follower, as per Section 5.2.
 	State ServerState
+	// ElectionTimeout is the current election timeout for the server. It is randomly chosen when the server is created.
+	// It should be used with a time.Timer, and the timer should be reset at the beginning of each new election and
+	// when the server receives an AppendEntries RPC, as per Section 5.2 from the
+	// [Raft paper](https://raft.github.io/raft.pdf). It only makes sense when Server is Follower or Candidate.
+	ElectionTimeout time.Duration
+}
+// TODO: Add the Transition Func
+
+type RaftServer struct {
+	raftpb.UnimplementedRaftRPCServer
+	State RaftState
+	// TODO: Design the Transport layer interface and the main_loop of the server (one goroutine iterating over channels and calling Transition)
 }
